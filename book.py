54e6c96cb5c700fd3e0938b7fd69b7d0646ff5fc
Utsav singh <utsavsingh62@gmail.com>
2020-03-04 12:56:04 +0530

    Add files via upload

--- book.py
+++ book.py
@@ -1,33 +1,109 @@
-class book:
-    def __init__(self, a='malgudi days', b='r.k narayan', c='penguin classic', d='$200'):
-        self.title=a
-        self.author=b
-        self.publisher=c
-        self.price=d
-    def get_price(self):
-        return self.price
-    def set_price(self, d):
-        self.price=d
-        return
-    def ShowInfo(self):
-        print('title:', self.title)
-        print('author:', self.author)
-        print('publisher:', self.publisher)
-        print('price:', self.price)
-    def royality(amount,copies,discount,retailprice):
-        amount=(discount*copies*retailprice/100)
-        return royality
-        copies=1
-        discount=10
-        retailprice=200
-        print("amount:")
-        copies=1000
-        discount=12.5
-        retailprice=200
-        print("amount:")
-        copies+=1000
-        discount=15
-        retailprice=200
-        print("amount:")
-        
-        
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'book.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(500, 400)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.button1 = QtWidgets.QPushButton(self.centralwidget)
+        self.button1.setGeometry(QtCore.QRect(350, 110, 75, 23))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        self.button1.setFont(font)
+        self.button1.setObjectName("button1")
+        self.button2 = QtWidgets.QPushButton(self.centralwidget)
+        self.button2.setGeometry(QtCore.QRect(350, 200, 75, 23))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        self.button2.setFont(font)
+        self.button2.setObjectName("button2")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(100, 110, 81, 20))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(9)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(100, 140, 71, 16))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(100, 200, 61, 16))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(9)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(100, 240, 131, 16))
+        self.label_4.setObjectName("label_4")
+        self.lineedit1 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineedit1.setGeometry(QtCore.QRect(182, 110, 151, 20))
+        self.lineedit1.setText("")
+        self.lineedit1.setObjectName("lineedit1")
+        self.lineedit2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineedit2.setGeometry(QtCore.QRect(180, 200, 151, 20))
+        self.lineedit2.setObjectName("lineedit2")
+        self.line = QtWidgets.QFrame(self.centralwidget)
+        self.line.setGeometry(QtCore.QRect(50, 170, 401, 20))
+        self.line.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line.setObjectName("line")
+        self.lineedit3 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineedit3.setGeometry(QtCore.QRect(180, 140, 113, 20))
+        self.lineedit3.setObjectName("lineedit3")
+        self.lineedit4 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineedit4.setGeometry(QtCore.QRect(180, 240, 113, 20))
+        self.lineedit4.setObjectName("lineedit4")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 500, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.button1.setText(_translate("MainWindow", "Find price"))
+        self.button2.setText(_translate("MainWindow", "Find total"))
+        self.label.setText(_translate("MainWindow", "Book title:"))
+        self.label_2.setText(_translate("MainWindow", "Price:"))
+        self.label_3.setText(_translate("MainWindow", "Quantity:"))
+        self.label_4.setText(_translate("MainWindow", "Total:"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
